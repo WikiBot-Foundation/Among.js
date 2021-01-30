039049ced3a0a2a94ce33aca00fd87d58951dcf5
impostor692 <77915398+impostor692@users.noreply.github.com>
2021-01-30 17:43:19 +0300

    AmongUsData

---
+++ AmongUsData.cs
@@ -0,0 +1,35 @@
+#<filed>
+#<data>
+      among.js(script.extensions(data.AmongUs)
+            among.js(script.extensions( == isJs)
+      Web.data(script.cs
+      
+            extensions="local"
+         data="python-extensions"
+                  Python.extensions(script.data)
+    Python.py(script.cs;local
+             ( == Extensions.py )
+   python.py(script.data == extension (cs.data)
+         
+             Python.data(py.script( == AmongUs
+
+       PlayerController.data(script.data)
+       Killbutton(KILL.isKILl(extension="kill"
+              Killbutton(KILL-roledata:Crewmate
+          $UpdateRole
+            Struct.data(killbutton.if(killbutton.clickdata
+            ifData(killthisPlayer).killPlayer.animation(script.data
+            
+   // Roles Are Updated. //
+             Script.data(role.ip(isport="player"
+                  Roles.(data.ip(port)
+          Among.js(roles.data.port(88.0.0.0.0)
+          Roles.data(port.88.0.0.0.0.0)
+                Playercontroller.ip(ifData.amongus(script.playergame)
+      AmongUs(script.88.0.0.0.0.0)
+              Role.ip(data(ifData.amongus(ipport)
+     
+
+
+
+  
