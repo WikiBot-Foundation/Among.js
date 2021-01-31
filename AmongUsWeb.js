e3044e0342659f29bc65e04a5f0d31478e8b2820
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-31 14:12:13 +0300

    AmongUs Web

---
+++ AmongUsWeb.js
@@ -0,0 +1,13 @@
+<script>
+        java/text(fuction)
+            viewer.api(AmongUsData)
+          ViewWeb="com.innersloth.spacemafia"
+       fiction == api* web( == inersloth == data
+                    }
+<script>
+           java == fiction(api). }
+         Body == data viewer == web
+                  (Data.server(api)
+       <view>
+     Hector == sequence.among.js(extensions)
+         viewer view == web(script)
