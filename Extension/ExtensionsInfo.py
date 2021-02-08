022fb5704d79c40757dd6acf23d7219c4998b8d6
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-08 22:41:42 +0300

    Create ExtensionsInfo.py

---
+++ Extension/ExtensionsInfo.py
@@ -0,0 +1,22 @@
+''''key load''''
+  ''"ExtensionName="Among.cs"''
+   "' first.key="java" '"'
+       "''version="1.1.8.22"'''
+          "''versionkey ==> log"'''
+    "'''extensionsPublicer ==> log"'''
+         input == log.file 
+         if : java(func.til ==> log
+        User.js(name.fuc
+               Extensi == log ==> log.func
+       Key.load(among.us(button ==> startTasks
+          "''' load ==> log(java.==> Func.t
+        
+    ""'''first.key( ==> myKey="2500"
+    ""'''first.key( ==> myKey="3921"
+        "''''key.name ==> my?==> "Py"
+     startTasks ==> func.my?tasks.bar
+            
+
+     
+        
+ 
