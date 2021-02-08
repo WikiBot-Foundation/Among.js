71cc75bedd35076426191657973ffb05d4e6ae0f
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-08 22:48:31 +0300

    Tasks

---
+++ Extension/Tasks.py
@@ -0,0 +1,14 @@
+<find>
+    Tasks ==> NAMETASKS
+     Tasks ==> JAVA
+           Func ==> Tasks
+   StartTasks;
+       tasksBar; LoadTasks.bar
+     tasks; ==> name.bar
+     Tasks ==> TestTasks
+            func.tasks(name.py)
+      java.name(tasks.bar
+            Tasks ==> JAVA
+        Tasks.if
+      if : alltasks(; completed == CrewmateWins.tasks
+      Tasks : bar(; load.completed
